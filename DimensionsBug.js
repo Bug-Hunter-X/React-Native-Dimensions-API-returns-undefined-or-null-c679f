6260927433a5f58f78307787ca0a5cb1cb491f81
BugHunter-X <askzenai@gmail.com>
2025-02-17 23:30:06 +0100

    Add DimensionsBug.js file

---
+++ DimensionsBug.js
@@ -0,0 +1,1 @@
+This error occurs when using the `Dimensions` API in React Native to get screen dimensions.  Sometimes, the dimensions might be reported as `undefined` or `null` unexpectedly, leading to layout issues or crashes.
